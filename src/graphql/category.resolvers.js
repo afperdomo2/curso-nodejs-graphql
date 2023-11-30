9eca223ee28edb2c6000fe02a3fc919daedf32d4
afperdomo2 <alvarado0925@gmail.com>
2023-11-30 17:42:41 -0500

    Implementa JWT en GraphQL en Category Resolver

--- src/graphql/category.resolvers.js
+++ src/graphql/category.resolvers.js
@@ -1,8 +1,13 @@
+const boom = require('@hapi/boom');
 const CategoryService = require('../services/category.service');
 
 const service = new CategoryService();
 
-const addCategory = (_, { data }) => {
+const addCategory = async (_, { data }, context) => {
+  const { user } = await context.authenticate('jwt', { session: false });
+  if (!user) {
+    throw boom.unauthorized('jwt is not valid');
+  }
   return service.create(data);
 };
 
