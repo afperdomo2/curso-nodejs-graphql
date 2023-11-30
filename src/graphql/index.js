4065a59d67b2db39b8316ec6e3eb1905848840f0
afperdomo2 <alvarado0925@gmail.com>
2023-11-30 17:01:11 -0500

    Implementa el Resolver del Login, con graphql-passport y la estrategia local-gql.strategy

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -5,6 +5,7 @@ const {
 const { expressMiddleware } = require('@apollo/server/express4');
 const { loadFiles } = require('@graphql-tools/load-files');
 const resolvers = require('./resolvers');
+const { buildContext } = require('graphql-passport');
 
 const useGraphql = async (app) => {
   const server = new ApolloServer({
@@ -19,7 +20,7 @@ const useGraphql = async (app) => {
 
   app.use(
     expressMiddleware(server, {
-      context: async ({ req }) => ({ token: req.headers.token }),
+      context: async ({ req, res }) => buildContext({ req, res }),
     })
   );
 };
